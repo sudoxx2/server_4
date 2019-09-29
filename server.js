e18e2b9535c6c7613a05e5baa40bba68f37530da
luv2c0de <moungpeter@gmail.com>
2019-09-29 12:49:31 -0700

    added base case for static html routing

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
-const con = require('./connection')
 const StudentRoutes = require('./students')
 const app = express();
+const path = require('path')
 
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/students', StudentRoutes);
 
-// app.use((req, res, next) => {
-//   console.log("HOLAMH");
-//   next();
-// })
+app.use((req, res, next) => {
+  console.log("HOLAMH");
+  next();
+})
 
-// app.get('/', (req, res) => {
-//   res.send("nani");
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/assets/views/', 'index.html'));
+});
 
 app.listen(3000);
