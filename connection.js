d6c82bab2675806740de916645b6993835cfcff0
sudoxx2 <moungpeter@gmail.com>
2019-09-29 00:53:17 -0700

    added endpoint add-students

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 var con = mysql.createConnection({
   host : 'localhost',
   user : 'root',
-  password : 'root',
+  password : 'Floorman11',
   database : 'mydb'
   // multipleStatements : true
 })
@@ -11,10 +11,16 @@ var con = mysql.createConnection({
 con.connect((err) => {
   if (err) throw err;
   console.log("Connected!");
-  // con.query("CREATE DATABASE mydb", function (err, result) {
-  //   if (err) throw err;
-  //   console.log("Database created");
-  // });
+  con.query("CREATE DATABASE IF NOT EXISTS mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+  con.query("CREATE TABLE IF NOT EXISTS Students (id int NOT NULL AUTO_INCREMENT, name varchar(255), age int, PRIMARY KEY (id))", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
 });
+
+
 
 module.exports = con;
--- students.js
+++ students.js
@@ -3,8 +3,9 @@ const Router = express.Router();
 const con = require('./connection');
 
 Router.get('/', (req, res) => {
-  con.query("SELECT * from students", (err, rows, fields) => {
+  con.query("SELECT * from Students", (err, rows, fields) => {
     if(!err) {
+      res.sendStatus(200);
       res.send(rows);
     } else {
       console.log(err);
@@ -13,8 +14,9 @@ Router.get('/', (req, res) => {
 })
 
 Router.post('/lookup-student', (req, res) => {
-  con.query(`SELECT * FROM students WHERE students.name = '${req.body.name}'`, (err, rows, fields) => {
+  con.query(`SELECT * FROM Students WHERE Students.name = '${req.body.name}'`, (err, rows, fields) => {
     if(!err) {
+      res.sendStatus(200);
       res.send(rows);
     } else {
       console.log(err);
@@ -23,13 +25,26 @@ Router.post('/lookup-student', (req, res) => {
 })
 
 Router.delete('/delete-student', (req, res) => {
-  con.query(`DELETE FROM students WHERE students.name = '${req.body.name}'`, (err, rows, fields) => {
+  con.query(`DELETE FROM Students WHERE Students.name = '${req.body.name}'`, (err, rows, fields) => {
     if(!err) {
+      res.sendStatus(200);
       res.send('deleted ' + req.body.name);
     } else {
       console.log(err);
     }
   })
+})
+
+Router.post('/add-student', (req, res) => {
+  con.query(`INSERT INTO Students (name, age) VALUES ('${req.body.name}', ${req.body.age})`, (err, rows, fields) => {
+    if(!err) {
+      // res.sendStatus(200);
+      res.send('added: ' + req.body.name);
+    } else {
+      console.log(err);
+    }
+  })
+  
 })
 
 
