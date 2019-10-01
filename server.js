b34fdb011c4d7f2a1847884a2bf6395dc94abb42
luv2c0de <moungpeter@gmail.com>
2019-09-30 22:20:11 -0700

    Finished JWT implementation

--- server.js
+++ server.js
@@ -1,21 +1,58 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const StudentRoutes = require('./students')
+const StudentRoutes = require('./students');
 const app = express();
-const path = require('path')
+const path = require('path');
 
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/students', StudentRoutes);
 
-app.use((req, res, next) => {
-  console.log("HOLAMH");
-  next();
-})
+// app.use((req, res, next) => {
+//   console.log("HOLAMH");
+//   next();
+// })
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/assets/views/', 'index.html'));
 });
+
+app.get('/secret', isAuthorized, (req, res) => {
+  res.sendFile(path.join(__dirname, '/assets/views/', 'index.html'));
+});
+
+
+app.get('/jwt', (req, res) => {
+
+  var signOptions = {
+    expiresIn:  "30s",
+    algorithm:  "HS256"   // RSASSA [ "RS256", "RS384", "RS512" ]
+   };
+
+  let privateKey = 'haxorz';
+  let token = jwt.sign({"body": "stuff"}, privateKey, signOptions);
+  res.send(token);
+})
+
+function isAuthorized(req, res, next){
+  if(typeof req.headers.authorization !== 'undefined') {
+    let token = req.headers.authorization.split(' ')[1];
+    let privateKey = 'haxorz';
+
+    jwt.verify(token, privateKey, {algorithm: 'HS256'}, (err, decoded) => {
+      if(err) {
+        res.status(500).json({error: "Not authorized"});
+      }
+
+      console.log(decoded);
+
+      return next();
+    })
+  } else {
+    res.status(500).json({error: "Not authorized"});
+  }
+}
 
 app.listen(3000);
