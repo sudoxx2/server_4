9cf8055244baca8cc66843bb9963b3e148b0fa23
luv2c0de <moungpeter@gmail.com>
2019-10-01 02:59:46 -0700

    cookie setup

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const StudentRoutes = require('./students');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/students', StudentRoutes);
-
+app.use(cookieParser());
 // app.use((req, res, next) => {
 //   console.log("HOLAMH");
 //   next();
@@ -17,6 +18,21 @@ app.use('/students', StudentRoutes);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/assets/views/', 'index.html'));
+});
+
+app.get('/cookie', (req, res) => {
+  res.cookie('myFirstCOokie', 'looks Good');
+  res.end('wow');
+});
+
+app.get('/cookie-req', (req, res) => {
+  console.log(req.cookies);
+  res.end('wow');
+});
+
+app.get('/cookie-remove', (req, res) => {
+  res.clearCookie('myFirstCOokie', 'looks Good');
+  res.end('wow');
 });
 
 app.get('/secret', isAuthorized, (req, res) => {
